414082b8feec5557ec85bd749fe05be1cb0a658f
evik246 <evastarish@gmail.com>
2023-06-01 10:31:29 +0300

    Created login page without api working

---
+++ BeautySalonSite/Models/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeautySalonSite.Models
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Укажите адрес электронной почты.")]
+        [EmailAddress(ErrorMessage = "Адрес электронной почты недействителен")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Укажите пароль")]
+        [PasswordPropertyText]
+        public string Password { get; set; } = string.Empty;
+    }
+}

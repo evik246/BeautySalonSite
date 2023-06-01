ee136f5fe113aad5de74eeb94133d7ed810c9952
evik246 <evastarish@gmail.com>
2023-06-01 19:18:51 +0300

    Added authorization

--- BeautySalonSite/Program.cs
+++ BeautySalonSite/Program.cs
@@ -1,7 +1,10 @@
 using BeautySalonSite;
 using BeautySalonSite.Shared;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
+using BeautySalonSite.AuthProviders;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +12,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7267/api/") });
 builder.Services.AddSingleton<SalonStateContainer>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();

38f4a9073f749bae90c0745f38477b5aefbe6ed1
evik246 <evastarish@gmail.com>
2023-06-01 10:55:41 +0300

    Added registration page without working api

---
+++ BeautySalonSite/Attributes/BirthdayAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeautySalonSite.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class BirthdayAttribute : ValidationAttribute
+    {
+        public int MinAge { get; set; }
+        public int MaxAge { get; set; }
+
+        public override bool IsValid(object? value)
+        {
+            if (value == null) { return true; }
+
+            if (value is DateTime date)
+            {
+                if (date.AddYears(MinAge) > DateTime.Now)
+                {
+                    return false;
+                }
+
+                return date.AddYears(MaxAge) > DateTime.Now;
+            }
+
+            if (value is DateOnly dateOnly)
+            {
+                DateTime dateTemp = dateOnly.ToDateTime(new TimeOnly());
+                if (dateTemp.AddYears(MinAge) > DateTime.Now)
+                {
+                    return false;
+                }
+
+                return dateTemp.AddYears(MaxAge) > DateTime.Now;
+            }
+            return false;
+        }
+    }
+}
---
+++ BeautySalonSite/Models/ClientRegistration.cs
@@ -0,0 +1,28 @@
+﻿using BeautySalonSite.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeautySalonSite.Models
+{
+    public class ClientRegistration
+    {
+        [Required(ErrorMessage = "Укажите имя")]
+        [StringLength(40, ErrorMessage = "Максимальная длина имени 40 символов")]
+        public string Name { get; set; } = string.Empty;
+
+        [Birthday(MinAge = 1, MaxAge = 100, ErrorMessage = "Дата рождения недействительна")]
+        public DateOnly? Birthday { get; set; }
+
+        [Required(ErrorMessage = "Укажите адрес электронной почты")]
+        [EmailAddress(ErrorMessage = "Адрес электронной почты недействителен")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Укажите пароль")]
+        [MinLength(8, ErrorMessage = "Пароль должен быть больше или равен 8 символам")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Укажите номер телефона")]
+        [Phone(ErrorMessage = "Номер телефона недействителен")]
+        [RegularExpression(@"\+[0-9]{12}", ErrorMessage = "Номер телефона недействителен")]
+        public string Phone { get; set; } = string.Empty;
+    }
+}

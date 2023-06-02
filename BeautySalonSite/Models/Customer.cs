9c71f326ffaf33f75f44de8a44eeb6d6e43a1cb5
evik246 <evastarish@gmail.com>
2023-06-02 18:29:36 +0300

    Added get client personal info

---
+++ BeautySalonSite/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace BeautySalonSite.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public DateOnly? Birthday { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+    }
+}
